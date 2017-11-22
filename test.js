d718ff387a4699805866a41f9072a346e9eb8a18
dien.tv <dien.tv@tctav.com>
2017-11-22 16:07:17 +0700

    update

--- test.js
+++ test.js
@@ -1,34 +1,15 @@
 var utils = require('./utils');
-var supertest = require("supertest");
-var should = require("should");
+var assert = require('chai').assert;
+const chai = require('chai');  
+const chaiHttp = require('chai-http');  
+chai.use(chaiHttp);
+var expect  = require('chai').expect;
 
-// This agent refers to PORT where the program is running.
-
-var server = supertest.agent("http://localhost:4000");
-
-// UNIT test begin
-
-var assert = require('assert');
-describe('Array', function() {
-  describe('#indexOf()', function() {
-    it('should return -1 when the value is not present', function() {
-      assert.equal(1, 1);
-    });
-  });
-});
-
-describe('Array', function() {
-  describe('Demo', function() {
-    it('Test fail', function() {
-      assert.equal(2, 2);
-    });
-  });
-});
-
-describe('43', function() {
-  describe('Util test', function() {
-    it('Test fail', function() {
+/*---Test API register*/
+describe('Test call APIs LineID', function() {
+     it('Test fail', function() {
       assert.equal(utils.getNumber(), 5);
     });
-  });
+    
+    //end
 });
